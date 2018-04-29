e3fb1436a2e9dff30476104dda1fdde02e27f5c2
Madeline Watson <mcwatson91@gmail.com>
2018-04-29 11:20:03 -0400

    set up start screen

---
+++ js/app.js
@@ -0,0 +1,7 @@
+!function () {
+  document.querySelector('body').innerHTML = '<div class="screen screen-start" id="start"><header><h1>Tic Tac Toe</h1><a href="#" class="button">Start game</a></header></div>';
+  // const button = document.querySelector(".button");
+  // button.onclick = function() {
+  //   document.querySelector('body').innerHTML =
+  // }
+}();

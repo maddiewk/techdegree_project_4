9a09efae02aa861c3406dedaeeda9b6921813024
Madeline Watson <mcwatson91@gmail.com>
2018-05-09 22:57:52 -0400

    testing game play

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
-// set up start page and activate button to set up game
-// trying to put all code inside this one immediately invoked function but we'll see
 
 !function () {
-  // first, hide game board and show start screen
+  // HTML variables
   const start = document.getElementById("start");
   const board = document.getElementById("board");
   const win = document.getElementById("finish");
@@ -10,12 +8,29 @@
   const playerOne = document.getElementById("player1");
   const playerTwo = document.getElementById("player2");
   const ul = document.querySelector(".boxes");
-  const boxes = document.querySelectorAll(".box");
+  let boxes = document.querySelectorAll(".box");
+  const message = document.querySelector(".message");
 
+// array to hold all possible winning combinations
+  let winCombinations = [
+    [boxes[0], boxes[1], boxes[2]],
+    [boxes[3], boxes[4], boxes[5]],
+    [boxes[6], boxes[7], boxes[8]],
+    [boxes[0], boxes[3], boxes[6]],
+    [boxes[1], boxes[4], boxes[7]],
+    [boxes[2], boxes[5], boxes[8]],
+    [boxes[0], boxes[4], boxes[8]],
+    [boxes[6], boxes[4], boxes[2]]
+  ];
+// arrays to hold each player's moves
+let playerOneMoves = [];
+let playerTwoMoves = [];
+
+  // hide game board and show start screen
   board.style.display = "none";
   win.style.display = "none";
 
-  // when the button is clicked, display the game board
+  // when the start button is clicked, display the game board
   startButton.onclick = function() {
     board.style.display = "";
     start.style.display = "none";
@@ -26,28 +41,60 @@
 
   // players trade turns
   // each time a square is clicked, it becomes disabled
-
   ul.addEventListener("click", (event) => {
     if (playerOne.classList.contains("active")) {
       playerTwo.className += " active";
       playerOne.className = "players";
       event.target.className += " box-filled-1 disabled";
+      playerOneMoves.push(event.target);
     } else if (playerTwo.classList.contains("active")) {
       playerOne.className += " active";
       playerTwo.className = "players";
       event.target.className += " box-filled-2 disabled";
+      playerTwoMoves.push(event.target);
     }
   });
 
   // highlight current player's symbol when mouse hovers over squares
-
 $(".box").hover(function(event) {
   if (playerOne.classList.contains("active")) {
-  $(this).addClass(" box-1").removeClass(" box-2");
+  $(this).addClass(" box-1").remove(" box-2");
 }
 if (playerTwo.classList.contains("active")) {
   $(this).addClass(" box-2").removeClass(" box-1");
 }
 });
+
+
+// add programming for win, lose, or draw
+// const winGame = (winCombinations, checkedBoxes) => {
+//   for (let i = 0; i < winCombinations.length; i++) {
+//     if (winCombinations[i].every(move => checkedBoxes.indexOf(move) != -1)) {
+//       return true;
+//     }
+//     return false;
+//     }
+//   }
+
+// show winner
+// const showWinner = (playerMoves) => {
+//   if (winGame(winCombinations, playerOneMoves) === true) {
+//     board.style.display = "none";
+//     win.style.display = "";
+//     message.innerHTML = "Congratulations! You won!";
+//   } else if (winGame(winCombinations, playerTwoMoves) === true) {
+//     board.style.display = "none";
+//     win.style.display = "";
+//     message.innerHTML = "You lost!";
+//   }
+// }
+
+// const showTie = (selectedBoxes) => {
+//   if ((selectedBoxes.length === 5) && (board.style.display === "block")) {
+//     board.style.display = "none";
+//     win.style.display = "";
+//     message.innerHTML = "It's a tie";
+//   }
+// }
 
 } ();
